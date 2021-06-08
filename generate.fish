900b76c10cd31420d44d26e10618609240380e64
Itsaka Rakotonirina <itsaka.rakotonirina@gmail.com>
2021-06-08 08:27:05 +0200

    restructuring repo

--- generate.fish
+++ generate.fish
@@ -1,11 +1,11 @@
 #!/usr/local/bin/fish
 
 function genheaders -a FILE
-  cat file_head.html $FILE file_tail.html
+  cat sources_html/file_head.html $FILE sources_html/file_tail.html
 end
 
-genheaders index_source.html > index.html 
-genheaders publications_source.html > publications.html
-genheaders cv_source.html > cv.html 
-genheaders projects_source.html > projects.html 
-genheaders slides_source.html > slides.html 
+genheaders sources_html/index_source.html > index.html 
+genheaders sources_html/publications_source.html > publications.html
+genheaders sources_html/cv_source.html > cv.html 
+genheaders sources_html/projects_source.html > projects.html 
+genheaders sources_html/slides_source.html > slides.html 
